1cf0b3f77253d4806b706f52affd98a37d6b202f
Li <yli2935@uwo.ca>
2023-06-10 13:18:21 -0400

    start main

---
+++ src/modules/pages/MainLayout.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import { Box, Toolbar, useMediaQuery } from '@mui/material';
+
