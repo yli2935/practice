a56295797633804822b1bc1461cbc7c3bc71f29e
Li <yli2935@uwo.ca>
2023-06-18 14:44:16 -0400

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: Li yli2935@uwo.ca
  * @Date: 2023-06-07 11:02:24
  * @LastEditors: Li yli2935@uwo.ca
- * @LastEditTime: 2023-06-18 14:42:03
+ * @LastEditTime: 2023-06-18 14:43:59
  * @FilePath: /practie/practice/src/App.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -24,6 +24,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/terms" element={<Terms />} />
+          <Route path="/main" element={<MainLayout />} />
           {/* <Route path="/products"  />
           <Route path="/sign-up"  /> */}
         </Routes>
