6ac23f075a13a9678f06b8c7a93506476a7a3d7e
Li <yli2935@uwo.ca>
2023-06-24 18:30:16 -0400

    public

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
  * @Author: Li yli2935@uwo.ca
  * @Date: 2023-06-07 11:02:24
  * @LastEditors: Li yli2935@uwo.ca
- * @LastEditTime: 2023-06-07 11:15:02
- * @FilePath: /practie/practice/src/index.js
+ * @LastEditTime: 2023-06-24 18:30:10
+ * @FilePath: /practie/practice/src/index.jsvv
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from "react";
