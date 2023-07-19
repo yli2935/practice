843b9a46bd003b6eea999bb1488488863af1125f
Li <yli2935@uwo.ca>
2023-07-19 15:17:24 -0400

    rebuild

--- src/modules/menu-items/index.js
+++ src/modules/menu-items/index.js
@@ -2,7 +2,7 @@
  * @Author: Li yli2935@uwo.ca
  * @Date: 2023-06-17 12:45:15
  * @LastEditors: Li yli2935@uwo.ca
- * @LastEditTime: 2023-06-17 14:09:05
+ * @LastEditTime: 2023-07-19 15:16:09
  * @FilePath: /practie/practice/src/modules/menu-items/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%A,
  */
@@ -15,7 +15,7 @@ import sort from './sort';
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = {
-  items: [introduction, linkedList, pages, stack,sort]
+  items: [introduction, linkedList, pages, stack, sort]
 };
 
 export default menuItems;
